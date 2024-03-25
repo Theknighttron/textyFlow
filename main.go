aa06a5423156f90c5fd9011410b12195c7065559
polyhistor <polyhistor@fancybear.fancybear>
2024-03-25 22:34:14 +0300

    Challenge 2 - how long customer have had their account

--- main.go
+++ main.go
@@ -3,18 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	messageFromDoris := []string{
-		"You doing anything later??",
-		"Did you get my last message?",
-		"Don't leave me hanging...",
-		"Please respond I'm lonely!",
-	}
-	numMessages := float64(len(messageFromDoris))
-	costPerMessage := .02
+	accountAge := 2.6
+	accountAgeInt := int(accountAge)
 
-
-	totalCost := costPerMessage * numMessages
-
-	fmt.Println(numMessages)
-	fmt.Printf("Doris spent %.2f on text messages today\n", totalCost)
+	fmt.Println("Your account has existed for", accountAgeInt, "years")
 }

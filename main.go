1e6f7c0701b68d528746799cb737bc80b31e2065
polyhistor2050 <chrisspaul2050@gmail.com>
2024-04-01 23:05:47 +0300

    Challenge 6 - Using nested struct

--- main.go
+++ main.go
@@ -1,29 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-// struct definition
-type messageToSend  struct{
-	PhoneNumber int
-	Message string
+// Using nested struct
+
+type messageToSend struct {
+	message string
+	sender user
+	recipient user
 }
 
-func test(text messageToSend) {
-	fmt.Printf("Sending message: '%s' to: %v\n", text.Message, text.PhoneNumber)
-	fmt.Println("==================================")
+type user struct {
+	name string
+	number int
 }
+
+func canSendMessage(mToSend messageToSend) bool {
+	if mToSend.sender.name == "" {
+		return false
+	}
+	if mToSend.recipient.name == "" {
+		return false
+	}
+	if mToSend.sender.number == 0 {
+		return false
+	}
+	if mToSend.recipient.number == 0 {
+		return false
+	}
+	return true
+}
+
+func test(mToSend messageToSend) {
+	fmt.Printf("sending '%s' from %s \n", mToSend.message, mToSend.sender.name)
+	fmt.Printf("%s (%v) to %s (%v)...\n", mToSend.sender.name, mToSend.sender.number, mToSend.recipient.name, mToSend.recipient.number)
+	fmt.Println("...sent! ")
+	fmt.Println("=======================================")
+}
+
 
 func main() {
 	test(messageToSend{
-		PhoneNumber: +255621458592,
-		Message: "Thanks for signing up",
+		message: "you have an appointment tommorow",
+		sender: user{name: "Carter", number: +255739683865},
+		recipient: user{name: "Tailor Jin", number: +255728694853},
 	})
 	test(messageToSend{
-		PhoneNumber: +255741859302,
-		Message: "Love to have you aboard",
+		message: "you have an event tommorow",
+		sender: user{name: "Suzan", number: +255784926519},
+		recipient: user{name: "James Peter", number: +255793029346},
 	})
 	test(messageToSend{
-		PhoneNumber: +2557392869204,
-		Message: "We're so excited to have you",
+		message: "you have a pary tommorow",
+		sender: user{number: +255638295723},
+		recipient: user{name: "Sally Sue"},
 	})
 }

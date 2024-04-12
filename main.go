bbdca8d2e8285c0fdd0ff4e8f399323d2e9414c5
polyhistor2050 <chrisspaul2050@gmail.com>
2024-04-12 13:54:45 +0300

    Challenge 7 - Interface and methods

--- main.go
+++ main.go
@@ -2,70 +2,50 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
-// Using nested struct
 
-type messageToSend struct {
-	message string
-	sender user
-	recipient user
+// Interface
+type message interface {
+	getMessage() string
 }
 
-type user struct {
-	name allNames
-	number int
+type birthdayMessage struct {
+	birthdayTime  time.Time
+	recipientName string
 }
 
-type allNames struct {
-	firstname string
-	lastname string
+type sendingReport struct {
+	reportName string
+	noOfSends  int
 }
 
-func canSendMessage(mToSend messageToSend) bool {
-	if mToSend.sender.name.firstname == "" {
-		return false
-	}
-	if mToSend.sender.name.lastname == "" {
-		return false
-	}
-	if mToSend.recipient.name.firstname == "" {
-		return false
-	}
-	if mToSend.recipient.name.lastname == "" {
-		return false
-	}
-	if mToSend.sender.number == 0 {
-		return false
-	}
-	if mToSend.recipient.number == 0 {
-		return false
-	}
-	return true
+func (bm birthdayMessage) getMessage() string {
+	return fmt.Sprintf("Hi %s, it is your birthday on %s \n", bm.recipientName, bm.birthdayTime)
 }
 
-func test(mToSend messageToSend) {
-	fmt.Printf("sending '%s' from %s \n", mToSend.message, mToSend.sender.name)
-	fmt.Printf("%s (%v) to %s (%v)...\n", mToSend.sender.name, mToSend.sender.number, mToSend.recipient.name, mToSend.recipient.number)
-	fmt.Println("...sent! ")
-	fmt.Println("=======================================")
+func (sr sendingReport) getMessage() string {
+	return fmt.Sprintf("Your %s report is ready. You've sent %v messages \n", sr.reportName, sr.noOfSends)
 }
 
+func sendMessage(msg message) {
+	fmt.Printf(msg.getMessage())
+}
+
+func test(m message) {
+	sendMessage(m)
+	fmt.Println("=====================================")
+}
 
 func main() {
-	test(messageToSend{
-		message: "you have an appointment tommorow",
-		sender: user{name: allNames{firstname: "Carter", lastname: "Smith"}, number: +255739683865},
-		recipient: user{name: allNames{firstname: "Tailor Jin"}, number: +255728694853},
+	test(sendingReport{
+		reportName: "First Report",
+		noOfSends: 10,
 	})
-	test(messageToSend{
-		message: "you have an event tommorow",
-		sender: user{name: allNames{firstname: "Suzan"}, number: +255784926519},
-		recipient: user{name: allNames{lastname: "James Peter"}, number: +255793029346},
+	test(birthdayMessage{
+		recipientName: "Carter James",
+		birthdayTime: time.Date(2000, 12,25, 0,0,0,0,time.UTC),
 	})
-	test(messageToSend{
-		message: "you have a pary tommorow",
-		sender: user{number: +255638295723},
-		recipient: user{name: allNames{lastname: "Sally Sue"}},
-	})
+
 }
